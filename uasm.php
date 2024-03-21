879f3e5fd80a2cc684e11bc2028ed39eb65ad905
sejas <antonio@sejas.es>
2024-03-21 17:21:00 +0000

    add some title and description to the plugin, to inform users what's happening with their site.

--- uasm.php
+++ uasm.php
@@ -11,6 +11,8 @@
  */
 
 function uasm_use_all_system_memory() {
+    echo "<h1>UASM - Unleash All System Memory <br></h1>";
+    echo "<p>Your site has installed the UASM plugin. Check the repository for more information. <a href='https://github.com/sejas/playground-plugin-use-all-system-memory'>https://github.com/sejas/playground-plugin-use-all-system-memory</a></p> <br><br>";
     echo "Initial memory usage: " . (memory_get_usage()/(1024*1024)) . ' MB <br>';
     $memoryUsage = 0;
     $data = '';
