5434c1086ff81fe8bad2139a61b061609475e609
sejas <antonio@sejas.es>
2024-03-21 17:21:37 +0000

    Simplify logic outputing the current memory_get_usage

--- uasm.php
+++ uasm.php
@@ -14,12 +14,10 @@ function uasm_use_all_system_memory() {
     echo "<h1>UASM - Unleash All System Memory <br></h1>";
     echo "<p>Your site has installed the UASM plugin. Check the repository for more information. <a href='https://github.com/sejas/playground-plugin-use-all-system-memory'>https://github.com/sejas/playground-plugin-use-all-system-memory</a></p> <br><br>";
     echo "Initial memory usage: " . (memory_get_usage()/(1024*1024)) . ' MB <br>';
-    $memoryUsage = 0;
     $data = '';
 
     while (true) {
         $data .= str_repeat('a', 1024 * 1024); // Increase string size by 1MB in each iteration
-        $memoryUsage = max($memoryUsage, memory_get_usage());
         echo "* " . (memory_get_usage()/(1024*1024)) . ' MB <br>';
     }
     die();
